566d3d90ccf1803dde3b994251a43b3df4d73a8a
stefanvanberkum <stefanvanberkum@gmail.com>
2021-12-02 23:43:53 +0100

    Updated code.

--- LSH.py
+++ LSH.py
@@ -25,10 +25,32 @@ def convert_binary(data):
         # (?:[^0-9\., ()]+[0-9]+) matches any (non numeric) - (numeric) combination.
         # (?:[0-9]+\.[0-9]+[^0-9\., ()]+) matches any (numeric) - . - (numeric) - (non-numeric) combination (i.e., decimals).
         # [a-zA-Z0-9]* matches any alphanumeric character (zero or more times).
-        # (?:$|(?=[ \)\]])) matches either the end of the string, trailing whitespace (exactly once), or a closing parenthesis or bracket (exactly once).
+        # (?:$|(?=[ \)\]])) matches either the end of the string, trailing whitespace, or a closing parenthesis or bracket (exactly once).
         mw_title = re.findall("(?:^|(?<=[ \[\(]))([a-zA-Z0-9]*(?:(?:[0-9]+[^0-9\., ()]+)|(?:[^0-9\., ()]+[0-9]+)|(?:[0-9]+\.[0-9]+[^0-9\., ()]+))[a-zA-Z0-9]*)(?:$|(?=[ \)\]]))", item["title"])
+        item_mw = mw_title
 
-        for mw in mw_title:
+        # Find model words in the key-value pairs.
+        features = item["featuresMap"]
+        for key in features:
+            value = features[key]
+
+            # Find decimals.
+            # (?:^|(?<=[ \[\(])) matches either the start of the string, preceding whitespace, or an opening parenthesis or bracket (exactly once).
+            # (?:[0-9]+\.[0-9]+[^0-9\., ()]+) matches any (numeric) - . - (numeric) - (non-numeric) combination (i.e., decimals).
+            # [a-zA-Z0-9]* matches any alphanumeric character (zero or more times).
+            # (?:$|(?=[ \)\]])) matches either the end of the string, trailing whitespace, or a closing parenthesis or bracket (exactly once).
+            mw_decimal = re.match("(?:^|(?<=[ \[\(]))((?:[0-9]+\.[0-9]+[^0-9\., ()]+)[a-zA-Z0-9]*)(?:$|(?=[ \)\]]))", value)
+            if not mw_decimal is None:
+                item_mw.append(mw_decimal.group(0))
+
+            # Find binary values, if "Yes" add the key as model word, if "No" add "no" + the key.
+            if value == "Yes":
+                item_mw.append(key)
+            elif value == "No":
+                item_mw.append("no" + key)
+
+        # Loop through all identified model words and update the binary vector product representation.
+        for mw in item_mw:
             if mw in model_words:
                 # Set index for model word to one.
                 row = model_words[mw]
@@ -40,8 +62,6 @@ def convert_binary(data):
 
                 # Add model word to the dictionary.
                 model_words[mw] = len(binary_vec) - 1
-
-        # Find model words in the key-value pairs.
 
     return binary_vec
 
